8d9393946b9c34905c21b366a53ee7773dc35941
Takashi-Na <Takashi-Na@users.noreply.github.com>
2020-09-24 11:59:32 +0900

    現在地取得機能 ver2

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,43 +1,22 @@
-// マップの初期表示設定
+// マップの表示設定
 let map
-// 初期位置の設定
 function initMap() {
-  geocoder = new google.maps.Geocoder()
-
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 35.6809591, lng: 139.7673068 },
+    // center: { lat: 35.6809591, lng: 139.7673068 }, これは東京駅
     zoom: 15,
   });
+
+  // 現在地の取得
+  let a = navigator.geolocation.getCurrentPosition(get_pos);
+  function get_pos(position) {
+    let lat = position.coords.latitude;　// 緯度
+    let lng = position.coords.longitude; // 経度
+    let latLng = new google.maps.LatLng(lat, lng);
+    const marker = new google.maps.Marker({　// 現在地の位置設定とアイコンのカスタマイズ
+      position: latLng,
+      map: map,
+    });
+    map.setCenter(latLng);　// 地図の中央に現在地を表示する
+  }
 }
 window.addEventListener('load', initMap);
-
-// 現在地取得
-function getMyPlace() {
-  const output = document.getElementById('result');
-  if (!navigator.geolocation) {//Geolocation apiがサポートされていない場合
-    output.innerHTML = "<p>Geolocationはあなたのブラウザーでサポートされておりません</p>";
-    return;
-  }
-  function success(position) {
-    var latitude = position.coords.latitude;//緯度
-    var longitude = position.coords.longitude;//経度
-    // 位置情報
-    var latlng = new google.maps.LatLng(latitude, longitude);
-    // Google Mapsに書き出し
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,// ズーム値
-      center: latlng,// 中心座標
-    });
-    // マーカーの新規出力
-    new google.maps.Marker({
-      map: map,
-      position: latlng,
-    });
-  };
-  function error() {
-    //エラーの場合
-    output.innerHTML = "座標位置を取得できません";
-  };
-  navigator.geolocation.getCurrentPosition(success, error);//成功と失敗を判断
-}
-window.addEventListener('load', getMyPlace);
